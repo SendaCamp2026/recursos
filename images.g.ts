f432fb065da0197144a879949b1665190ede321b
SendaCamp2026 <sendacamp2026@gmail.com>
2026-07-09 20:45:42 -0700

    Archivos iniciales del proyecto de MakeCode

---
+++ images.g.ts
@@ -0,0 +1,157 @@
+// Código generado automáticamente. No editar.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "image1":
+            case "llave1":return img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . f f f . . 
+. . . . . . . . . . f 5 5 5 f . 
+. f f f f f f f f f 5 5 f 5 5 f 
+f 5 5 5 5 5 5 5 5 5 5 f . f 5 f 
+f 5 f 5 f 5 f f f f 5 5 f 5 5 f 
+. f . f . f . . . . f 5 5 5 f . 
+. . . . . . . . . . . f f f . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`;
+            case "image2":
+            case "llave2":return img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . f f f . . 
+. . . . . . . . . . f 5 5 5 f . 
+f f f f f f f f f f 5 5 5 5 5 f 
+f 5 5 5 5 5 5 5 5 5 5 5 f 5 5 f 
+f 5 f 5 f f f f f f 5 5 5 5 5 f 
+f f . f f . . . . . f 5 5 5 f . 
+. . . . . . . . . . . f f f . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`;
+            case "image3":
+            case "llave3":return img`
+. . . . . . . f f . . . . . . . 
+. . . . . . f 5 5 f . . . . . . 
+. . . . . f 5 a a 5 f . . . . . 
+. . . . f 5 5 a a 5 5 f . . . . 
+. . . f 5 5 5 5 5 5 5 5 f . . . 
+. . . f 5 5 5 f f 5 5 5 f . . . 
+. . . f 5 5 f . . f 5 5 f . . . 
+. . . . f 5 f f f f 5 f . . . . 
+. . . . . f 5 5 5 5 f . . . . . 
+. . . . . . f 5 5 f . . . . . . 
+. . . . . . f 5 5 f . . . . . . 
+. . . . . . f 5 5 f f f . . . . 
+. . . . . . f 5 5 f 5 5 f . . . 
+. . . . . . f 5 5 5 5 f . . . . 
+. . . . . . f 5 5 f 5 5 f . . . 
+. . . . . . . f f . f f . . . . 
+`;
+            case "image4":
+            case "cajaSecreta":return img`
+. . . . . . . . . . . . . . . . 
+. f f f f f f f f f f f f f f . 
+. f 5 5 5 5 5 5 5 5 5 5 5 5 f . 
+. f 5 5 5 f f f f f f 5 5 5 f . 
+. f 5 5 f f 5 5 5 5 f f 5 5 f . 
+. f 5 5 f 5 5 5 5 5 f f 5 5 f . 
+. f 5 5 5 5 5 5 5 5 f f 5 5 f . 
+. f 5 5 5 5 5 5 5 5 f 5 5 5 f . 
+. f 5 5 5 5 5 5 5 f f 5 5 5 f . 
+. f 5 5 5 5 5 5 f f 5 5 5 5 f . 
+. f 5 5 5 5 5 f f 5 5 5 5 5 f . 
+. f 5 5 5 5 5 5 5 5 5 5 5 5 f . 
+. f 5 5 5 5 5 f f 5 5 5 5 5 f . 
+. f 5 5 5 5 5 5 5 5 5 5 5 5 f . 
+. f f f f f f f f f f f f f f . 
+. . . . . . . . . . . . . . . . 
+`;
+            case "image5":
+            case "CajaSorpresa2":return img`
+. . . . . . . . . . . . . . . . 
+. . . . f f f f f f f f f f . . 
+. . . f f f f f f f f f f f . . 
+. . f f 5 5 5 5 5 5 5 5 f f . . 
+. f f 5 5 5 5 5 5 5 5 f f f . . 
+. f f f f f f f f f f f 5 f . . 
+. f 5 5 5 5 5 5 5 f f 5 5 f . . 
+. f 5 5 f f f 5 5 f 5 5 5 f . . 
+. f 5 f 5 5 5 f 5 f 5 5 5 f . . 
+. f 5 5 5 5 5 f 5 f 5 5 5 f . . 
+. f 5 5 5 5 f 5 5 f 5 5 5 f . . 
+. f 5 5 5 f 5 5 5 f 5 5 f f . . 
+. f 5 5 5 5 5 5 5 f 5 f f . . . 
+. f 5 5 5 f 5 5 5 f f f . . . . 
+. f f f f f f f f f f . . . . . 
+. . . . . . . . . . . . . . . . 
+`;
+            case "image6":
+            case "Moneda":return img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . f f f f f f f . . . . . 
+. . . f 5 5 5 f 5 5 5 f . . . . 
+. . f 5 5 5 f f f f 5 5 f . . . 
+. . f 5 5 f 5 f 5 5 5 5 f . . . 
+. . f 5 5 f 5 f 5 5 5 5 f . . . 
+. . f 5 5 5 f f f 5 5 5 f . . . 
+. . f 5 5 5 5 f 5 f 5 5 f . . . 
+. . f 5 5 5 5 f 5 f 5 5 f . . . 
+. . f 5 5 f f f f 5 5 5 f . . . 
+. . . f 5 5 5 f 5 5 5 f . . . . 
+. . . . f f f f f f f . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`;
+            case "image7":
+            case "moneda2":return img`
+. . b b b b . . 
+. b 5 5 5 5 b . 
+b 5 d 3 3 d 5 b 
+b 5 3 5 5 1 5 b 
+c 5 3 5 5 1 d c 
+c d d 1 1 d d c 
+. f d d d d f . 
+. . f f f f . . 
+`;
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+    helpers._registerFactory("song", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+    helpers._registerFactory("json", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// Código generado automáticamente. No editar.
